894bfa8323f84327ec2ccffdef539539485c93a8
Samantha0311 <samyakerman0@gmail.com>
2025-06-18 13:02:35 -0400

    inicio de innstruction por size erres en lexer y token revira

--- sln/ast/Lexer.cs
+++ sln/ast/Lexer.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 
 namespace WallyArt.sln.ast
 {
+    /* Convierte el teto del editor en una lista de tokens */
     internal class Lexer
     {
         public string[] lines;
@@ -14,58 +15,61 @@ namespace WallyArt.sln.ast
 
         public Lexer(string code)
         {
-            lines = code.Split( '\n' );
+            lines = code.Split('\n');
             lineNumber = 0;
+        }
 
-            List<Token> Tokenize()
+        /* Extrae todos los tokens de todas las lineas */
+        public  List<Token> Tokenize()
+        {
+            var tokens = new List<Token>();
+            foreach (var line in lines)
             {
-                var tokens = new List<Token>();
-                foreach(var line in lines)
+                lineNumber++;
+                string trimeed = line.Trim();
+
+                if (trimeed == "")
                 {
-                    lineNumber++;
-                    string trimeed = line.Trim();
-
-                    if(trimeed == "")
-                    {
-                        continue;
-                    }
-
-                    var parts = Regex.Matches(trimeed, "\".*?\"|[a-zA-Z_][\\w\\-]*|\\d+|<-|==|<=|>=|!=|[+\\-*/%()\\[\\],]");         
-
-                    foreach(Match part in parts)
-                    {
-                        string val = part.Value;
-
-                        if(int.TryParse(val, out _))
-                        {
-                            tokens.Add(new Token(TokenTipe.Number, val, lineNumber));
-                        }
-                        else if (val.StartsWith("\""))
-                        {
-                            tokens.Add(new Token(TokenTipe.String, val.Trim('"'), lineNumber));
-                        }
-                        else if (val == "<-" || val == "==" || val == "<=" || val == ">=" || val == "<" || val == ">")
-                        {
-                            tokens.Add(new Token(TokenTipe.Operator, val, lineNumber));
-                        }
-                        else if ("(),[]".Contains(val))
-                        {
-                            tokens.Add(new Token(TokenTipe.Symbol, val, lineNumber));
-                        }
-                        else if (Regex.IsMatch(val, "^[a-zA-Z_]\\w*$"))
-                        {
-                            tokens.Add(new Token(TokenTipe.Identifier, val, lineNumber));
-                        }
-                        else
-                        {
-                            throw new Exception($"Unknwn token '{val}' at line {lineNumber}");
-                        }
-                    }
+                    continue;
                 }
 
-                tokens.Add(new Token(TokenTipe.EOF, "EOF", lineNumber));
-                return tokens;
+                /* Separa en partes usando un exprecion regular */
+                var parts = Regex.Matches(trimeed, "\".*?\"|[a-zA-Z_][\\w\\-]*|\\d+|<-|==|<=|>=|!=|[+\\-*/%()\\[\\],]");
+
+                foreach (Match part in parts)
+                {
+                    string val = part.Value;
+
+                    if (int.TryParse(val, out _))
+                    {
+                        tokens.Add(new Token(TokenTipe.Number, val, lineNumber));
+                    }
+                    else if (val.StartsWith("\""))
+                    {
+                        tokens.Add(new Token(TokenTipe.String, val.Trim('"'), lineNumber));
+                    }
+                    else if (val == "<-" || val == "==" || val == "<=" || val == ">=" || val == "<" || val == ">")
+                    {
+                        tokens.Add(new Token(TokenTipe.Operator, val, lineNumber));
+                    }
+                    else if ("(),[]".Contains(val))
+                    {
+                        tokens.Add(new Token(TokenTipe.Symbol, val, lineNumber));
+                    }
+                    else if (Regex.IsMatch(val, "^[a-zA-Z_]\\w*$"))
+                    {
+                        tokens.Add(new Token(TokenTipe.Identifier, val, lineNumber));
+                    }
+                    else
+                    {
+                        throw new Exception($"Unknwn token '{val}' at line {lineNumber}");
+                    }
+                }
             }
+
+            tokens.Add(new Token(TokenTipe.EOF, "EOF", lineNumber));  /* Token especial de fin */
+            return tokens;
         }
+        
     }
 }
---
+++ sln/instructions/Instruction.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WallyArt.sln.context;
+
+namespace WallyArt.sln.instructions
+{
+    public abstract class Instruction
+    {
+        public int Line;
+        public abstract void Execute(Context context);
+
+        public Instruction(int line)
+        {
+            Line = line;
+        }
+    }
+
+    public class SwapI : Instruction
+    {
+        public int X;
+        public int Y;
+        
+        public SwapI(int x, int y, int line) : base(line) 
+        {
+            X = x; Y = y; 
+        }
+        public override void Execute(Context context)
+        {
+           
+        }
+    }
+
+    public class ColorI : Instruction
+    {
+        public string Color;
+        public ColorI(string color, int line) : base(line)
+        {
+            Color = color;
+        }
+        public override void Execute(Context context)
+        {
+            
+        }
+    }
+
+    public class DrawLineI : Instruction
+    {
+        public DrawLineI( , int line) : base(line) 
+        {
+
+        }
+
+        public override void Execute(Context context)
+        {
+            
+        }
+    }
+
+    public class ZyseI : Instruction
+    {
+        public ZyseI( , int line) : base(line)
+        {
+
+        }
+
+        public override void Execute(Context context)
+        {
+            
+        }
+    }
+}
